0e51eb0521d4dc450d33d24d00bca945acaa8f6e
deejay9424 <dhruv.voith@gmail.com>
2019-06-11 22:43:27 +0530

    mongo

--- server/config/mongooseConfig.js
+++ server/config/mongooseConfig.js
@@ -7,16 +7,14 @@ var port = mongoConfig.port;
 mongoose.connect(dbName, { useNewUrlParser: true });
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(chalk.yellow('connection error...')));
-db.on('connected', () => {
-    if (process.env.NODE_ENV == 'DEV') {
-        console.log(chalk.yellow('Database opened on port : ' + port));
-    }
-});
-db.on('disconnected', () => {
-    if (process.env.NODE_ENV == 'DEV') {
-        console.log(chalk.yellow('Database is disconnected'));
-    }
+// db.on('error', console.error.bind(chalk.yellow('connection error...')));
+// db.on('connected', () => {
+//         console.log(chalk.yellow('Database opened on port : ' + port));
+// });
+// db.on('disconnected', () => {
+//     if (process.env.NODE_ENV == 'DEV') {
+//         console.log(chalk.yellow('Database is disconnected'));
+//     }
 
-});
+// });
 module.exports = db;
